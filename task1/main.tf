81e2e34a167f5d093170d33ebff869aef3e8b9c8
Olena Hrynko <olena.hrynko.pp.2022@lpnu.ua>
2026-04-05 11:42:16 +0300

    Update main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -13,6 +13,9 @@ terraform {
     key                         = "terraform.tfstate"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    use_path_style
   }
 }
 
