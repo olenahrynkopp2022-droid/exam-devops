e2c5262691209dee916418d179e0e3714300b90c
Olena Hrynko <olena.hrynko.pp.2022@lpnu.ua>
2026-04-05 11:37:18 +0300

    Refactor DigitalOcean resources and update firewall rules

--- task1/main.tf
+++ task1/main.tf
@@ -1,71 +1,71 @@
-provider "digitalocean" {
-  token = var.do_token
-  
-  spaces_access_id  = var.spaces_access_key
-  spaces_secret_key = var.spaces_secret_key
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+
+  backend "s3" {
+    endpoint                    = "fra1.digitaloceanspaces.com"
+    region                      = "us-east-1" 
+    bucket                      = "hrynko-bucket"
+    key                         = "terraform.tfstate"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
 }
 
-resource "digitalocean_vpc" "vpc" {
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# 1. VPC
+resource "digitalocean_vpc" "hrynko_vpc" {
   name     = "hrynkoo-vpc"
   region   = "fra1"
   ip_range = "10.10.10.0/24"
 }
 
-resource "digitalocean_droplet" "node" {
-  name   = "hrynkoo-node"
-  region = "fra1"
-  size   = "s-2vcpu-4gb"
-  image  = "ubuntu-24-04-x64"
-
-  vpc_uuid = digitalocean_vpc.vpc.id
+# 2. ВМ (Droplet)
+resource "digitalocean_droplet" "hrynko_node" {
+  name     = "hrynkoo-node"
+  region   = "fra1"
+  size     = "s-2vcpu-4gb" 
+  image    = "ubuntu-24-04-x64"
+  vpc_uuid = digitalocean_vpc.hrynko_vpc.id
   ssh_keys = [var.ssh_fingerprint]
 }
 
-resource "digitalocean_firewall" "fw" {
-  name = "hrynko-firewall"
-  droplet_ids = [digitalocean_droplet.node.id]
+# 3. Фаєрвол
+resource "digitalocean_firewall" "hrynko_firewall" {
+  name        = "hrynkoo-firewall"
+  droplet_ids = [digitalocean_droplet.hrynko_node.id]
 
   inbound_rule {
     protocol         = "tcp"
     port_range       = "22"
-    source_addresses = ["0.0.0.0/0"]
+    source_addresses = ["0.0.0.0/0", "::/0"]
   }
-
   inbound_rule {
     protocol         = "tcp"
     port_range       = "80"
-    source_addresses = ["0.0.0.0/0"]
+    source_addresses = ["0.0.0.0/0", "::/0"]
   }
-
   inbound_rule {
     protocol         = "tcp"
     port_range       = "443"
-    source_addresses = ["0.0.0.0/0"]
+    source_addresses = ["0.0.0.0/0", "::/0"]
   }
-
   inbound_rule {
     protocol         = "tcp"
     port_range       = "8000-8003"
-    source_addresses = ["0.0.0.0/0"]
+    source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   outbound_rule {
     protocol              = "tcp"
     port_range            = "1-65535"
-    destination_addresses = ["0.0.0.0/0"]
+    destination_addresses = ["0.0.0.0/0", "::/0"]
   }
-}
-
-resource "digitalocean_spaces_bucket" "bucket" {
-  name   = "hrynko-bucket"
-  region = "fra1"
-}
-
-variable "do_token" {}
-variable "ssh_fingerprint" {}
-variable "spaces_access_key" {}
-variable "spaces_secret_key" {}
-
-output "ip" {
-  value = digitalocean_droplet.node.ipv4_address
 }
